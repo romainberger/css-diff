f4abd719de26fa71519ae6dc8d0754bf9f1a2cb1
sihyeon <sihyeon@himedi.com>
2022-02-28 14:08:30 +0900

    fix error
    if use multi selector (el, el {}) somtimes make refer undefined error

--- src/index.js
+++ src/index.js
@@ -1,93 +1,90 @@
-import {parse as postcssParse} from 'postcss'
-import merge from 'lodash.merge'
+import { parse as postcssParse } from "postcss";
+import merge from "lodash.merge";
 
 const parse = (css) => {
-  const ast = postcssParse(css)
-  let result = {}
+  const ast = postcssParse(css);
+  let result = {};
 
-  ast.nodes.forEach(node => {
-    if (node.type === 'rule') {
-      const declarations = {}
+  ast.nodes.forEach((node) => {
+    if (node.type === "rule") {
+      const declarations = {};
 
-      node.nodes.forEach(dcl => {
-        if (dcl.type !== 'decl') {
-          return
+      node.nodes.forEach((dcl) => {
+        if (dcl.type !== "decl") {
+          return;
         }
-        declarations[dcl.prop] = dcl.value
-      })
+        declarations[dcl.prop] = dcl.value;
+      });
 
-      result = merge(result, {[node.selector]: declarations})
+      result = merge(result, { [node.selector]: declarations });
     }
-  })
+  });
 
-  return result
-}
+  return result;
+};
 
 const toString = (css) => {
-  let result = ''
+  let result = "";
 
-  Object.keys(css).forEach(selector => {
-    result = `${result}${selector} {\n`
+  Object.keys(css).forEach((selector) => {
+    result = `${result}${selector} {\n`;
 
-    Object.keys(css[selector]).forEach(prop => {
-      result = `${result}  ${prop}: ${css[selector][prop]};\n`
-    })
+    Object.keys(css[selector]).forEach((prop) => {
+      result = `${result}  ${prop}: ${css[selector][prop]};\n`;
+    });
 
-    result = `${result}}\n`
-  })
+    result = `${result}}\n`;
+  });
 
-  return result
-}
+  return result;
+};
 
 const addProp = (diff, selector, prop, value) => {
   if (diff[selector]) {
-    diff[selector][prop] = value
-  }
-  else {
+    diff[selector][prop] = value;
+  } else {
     diff[selector] = {
       [prop]: value,
-    }
+    };
   }
 
-  return diff
-}
+  return diff;
+};
 
 const cssDiff = (source, reversed) => {
-  var isStringified = false
+  var isStringified = false;
 
   try {
-    source = JSON.parse(source)
-    reversed = JSON.parse(reversed)
-    isStringified = true
-  }
-  catch (e) {}
+    source = JSON.parse(source);
+    reversed = JSON.parse(reversed);
+    isStringified = true;
+  } catch (e) {}
 
-  const sourceObject = parse(source)
-  const reversedObject = parse(reversed)
-  let diff = {}
+  const sourceObject = parse(source);
+  const reversedObject = parse(reversed);
+  let diff = {};
 
-  Object.keys(reversedObject).forEach(selector => {
-    Object.keys(reversedObject[selector]).forEach(prop => {
-      if (sourceObject[selector][prop]) {
+  Object.keys(reversedObject).forEach((selector) => {
+    Object.keys(reversedObject[selector]).forEach((prop) => {
+      if (sourceObject[selector] && sourceObject[selector][prop]) {
         if (sourceObject[selector][prop] !== reversedObject[selector][prop]) {
-          diff = addProp(diff, selector, prop, reversedObject[selector][prop])
+          diff = addProp(diff, selector, prop, reversedObject[selector][prop]);
         }
+      } else {
+        diff = addProp(diff, selector, prop, reversedObject[selector][prop]);
       }
-      else {
-        diff = addProp(diff, selector, prop, reversedObject[selector][prop])
-      }
-    })
-  })
+    });
+  });
 
-  diff = toString(diff)
+  diff = toString(diff);
 
   if (isStringified) {
-    diff = JSON.stringify(diff)
+    diff = JSON.stringify(diff);
   }
 
-  return diff
-}
+  return diff;
+};
 
-module.exports = cssDiff
-module.exports.parse = parse
-module.exports.toString = toString
+module.exports = cssDiff;
+module.exports.parse = parse;
+module.exports.toString = toString;
